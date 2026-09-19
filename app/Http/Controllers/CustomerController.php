a096ad0a6df0b356d9d992848631ee57e24fb669
Natania Setyawati <natania.nn64@gmail.com>
2026-09-20 01:53:46 +0800

    Add CustomerController and API Routes

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Customer::query();
+
+        if ($request->has('status')) {
+            $query->where('status', $request->status);
+        }
+
+        if ($request->has('search')) {
+            $query->where('name', 'like', '%' . $request->search . '%');
+        }
+
+        $customers = $query->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $customers,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Customer not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $customer,
+        ]);
+    }
+}
